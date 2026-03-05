8c34b192ca974a035b815573f519ec9d927cf5da
Jay Koo <jaykoo@iMac.local>
2026-03-06 00:27:57 +0900

    Add shortcut icon link for TopStar favicon

--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -21,6 +21,7 @@ export function renderHomeworkHtml({ pageTitle }) {
     <meta name="apple-mobile-web-app-title" content="${title}" />
     <link rel="manifest" href="./manifest.webmanifest" />
     <link rel="icon" href="./icon.png" type="image/png" />
+    <link rel="shortcut icon" href="./icon.png" type="image/png" />
     <link rel="icon" href="./icon.svg" type="image/svg+xml" />
     <link rel="apple-touch-icon" href="./icon.png" />
     <style>
